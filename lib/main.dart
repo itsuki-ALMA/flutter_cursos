79e535f0df691d8f6f1b91e93badefe377a3defc
itsuki-ALMA <alexconstantino2909@gmail.com>
2025-07-21 08:12:50 -0300

    Revert "[TT-01] login screen and rout logic created"

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cursos/ui/home_screen.dart';
-import 'package:flutter_cursos/ui/login_screen.dart';
 
 void main() {
-  runApp(const TempoCursaApp());
+  runApp(const MainApp());
 }
 
-class TempoCursaApp extends StatelessWidget {
-  const TempoCursaApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        "login": (context) => const LoginScreen(),
-        "home": (context) => HomeScreen(),
-      },
-      initialRoute: "login",
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
     );
   }
-} 
+}
